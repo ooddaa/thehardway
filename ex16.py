965433e7d99182168488cab658edf2063ee24aeb
oda <ooddaa@gmail.com>
2022-04-10 22:54:26 +0100

    ex50

--- ex16.py
+++ ex16.py
@@ -5,7 +5,8 @@ script, filename = argv
 print(f"Now we're going to empty {filename}")
 print("Do you want that? Hit CTRL-C (^C) if 'no'")
 print("Hit RETURN if 'yes'")
-input('? ')
+
+input('> ')
 file = open(filename, 'w')
 print(f"OK, let's empty it then")
 file.truncate()
@@ -24,6 +25,13 @@ file.write(line3)
 file.write("\n")
 
 print(f"Is it there?")
-file.close()
-newF = open(filename)
-print(newF.read())
+answer = input('(y)/(N)> ')
+# if answer == 'y' or 'Y' or '(y)' or 'yes':
+if answer == 'y' or answer == 'Y' or answer == '(y)' or answer == 'yes':
+    print('all ok, thanks')
+    file.close()
+    # newF = open(filename)
+    # print(newF.read())
+else:
+    print('I didn\'t expect that. Not implemented. closing down')
+    file.close()
--- ex19.py
+++ ex19.py
@@ -1,33 +1,45 @@
-def cheese_and_crackers(cheese_count, boxes_of_crackers):
-	print(f"You have {cheese_count} cheeses!")
-	print(f"You have {boxes_of_crackers} boxes of crackers!")
-	print(f"Man that's enough for a party!")
-	print(f"Get a blanket. \n")
-	
-print(f"giving function numbers directly:")
-cheese_and_crackers(10, 20)
+# def cheese_and_crackers(cheese_count, boxes_of_crackers):
+# 	print(f"You have {cheese_count} cheeses!")
+# 	print(f"You have {boxes_of_crackers} boxes of crackers!")
+# 	print(f"Man that's enough for a party!")
+# 	print(f"Get a blanket. \n")
 
-print(f"OR we could use variables from our script:")
-cheese = 20
-crackers = 34
-cheese_and_crackers(cheese, crackers)
+# print(f"giving function numbers directly:")
+# cheese_and_crackers(10, 20)
 
-print(f"or just do math:")
-cheese_and_crackers(3**2, 20 + 3)
+# print(f"OR we could use variables from our script:")
+# cheese = 20
+# crackers = 34
+# cheese_and_crackers(cheese, crackers)
 
-print(f"OR even combine varables and some math inside:")
-cheese_and_crackers(cheese*10, crackers // 4)
+# print(f"or just do math:")
+# cheese_and_crackers(3**2, 20 + 3)
 
-def lol():
-	print("IT WOOORKS!")
+# print(f"OR even combine varables and some math inside:")
+# cheese_and_crackers(cheese*10, crackers // 4)
+
+# def lol():
+# 	print("IT WOOORKS!")
 
 
-dict = {'lol': lol}
-def runner(fn):
-	if fn in dict:
-		return dict[fn]()
-	print(f"I don't know what to do with {fn}")
+# dict = {'lol': lol}
+# def runner(fn):
+# 	if fn in dict:
+# 		return dict[fn]()
+# 	print(f"I don't know what to do with {fn}")
 
-user_function = input("what's your function?")
+# user_function = input("what's your function?")
 
-runner(user_function)
+# runner(user_function)
+
+def a_hundred():
+    100
+
+
+def mega_function(a, b):
+    return print(a + b)
+
+
+mega_function(100, 20)  # 120
+
+mega_fuction(a_hundred(), 20)
